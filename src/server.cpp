15ea26f466d07831de7d2700d4e93406e85b5693
richardhenryhopper <richardhenryhopper@gmail.com>
2024-04-07 20:45:31 +0100

    Adding server files.

--- src/server.cpp
+++ src/server.cpp
@@ -1,17 +1,13 @@
 #include "server.h"
 
-IncubatorServer::IncubatorServer()
-{
-   
-}
-/// @brief 
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(HTTP_PORT);
+// Create websocket
+AsyncWebSocket ws("/ws");
 
-void IncubatorServer::begin(){
-}
-
-void IncubatorServer::initWiFi() {
+void serverInitWiFi() {
     WiFi.mode(WIFI_STA);
-    WiFi.begin(wifiSSID, wifiPass);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
@@ -20,24 +16,26 @@ void IncubatorServer::initWiFi() {
     Serial.printf(" %s\n", WiFi.localIP().toString().c_str());
 }
 
-void IncubatorServer::initWebServer() {
-    _server = new AsyncWebServer(HTTP_PORT);
-    _server->on("/", HTTP_GET, [](AsyncWebServerRequest *request){ 
-        request->send(SPIFFS, "/index.html", "text/html");
-    });
-    _server->serveStatic("/", SPIFFS, "/");
-    _server->begin();
+void serverInitWebServer() {
+    server.on("/", HTTP_GET, serverOnRootRequest);
+    server.serveStatic("/", SPIFFS, "/");
+    server.begin();
 }
 
-void IncubatorServer::initWebSocket() {
-    _ws = new AsyncWebSocket("/ws");
-    _ws->onEvent([this](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-        onEvent(server, client, type, arg, data, len);
-    });
-    _server->addHandler(_ws); 
+void serverInitWebSocket() {
+    ws.onEvent(serverWebsocketOnEvent);
+    server.addHandler(&ws); 
 }
 
-void IncubatorServer::onEvent(AsyncWebSocket       *server,
+void serverOnRootRequest(AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html", "text/html");
+}
+
+void serverNotifyWebsocketClients(String message) {
+    ws.textAll(message);
+}
+
+void serverWebsocketOnEvent(AsyncWebSocket       *server,
              AsyncWebSocketClient *client,
              AwsEventType          type,
              void                 *arg,
@@ -52,7 +50,7 @@ void IncubatorServer::onEvent(AsyncWebSocket       *server,
             Serial.printf("WebSocket client #%u disconnected\n", client->id());
             break;
         case WS_EVT_DATA:
-            handleWebSocketMessage(arg, data, len);
+            serverHandleWebSocketMessage(arg, data, len);
             break;
         case WS_EVT_PONG:
         case WS_EVT_ERROR:
@@ -60,19 +58,10 @@ void IncubatorServer::onEvent(AsyncWebSocket       *server,
     }
 }
 
-//void Incubator::onRootRequest(AsyncWebServerRequest *request) {
-  //request->send(SPIFFS, "/index.html", "text/html");
-//}
-
-void IncubatorServer::handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+void serverHandleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     AwsFrameInfo *info = (AwsFrameInfo*)arg;
     if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
         Serial.println("test");
     }
 }
-
-void IncubatorServer::notifyClients(String message) {
-    _ws->textAll(message);
-}
-
 
--- src/server.h
+++ src/server.h
@@ -9,30 +9,27 @@
 
 #define HTTP_PORT 80
 
-class IncubatorServer {
-    public:
-        IncubatorServer();
-        void begin();
-        const char *wifiSSID = "Makespace";
-        const char *wifiPass = "getexc1tedandmaketh1ngs";
-        
-    private:
-        void initWiFi();
-        void initWebServer();
-        void initWebSocket();
-        bool initSensor();
-        //void onRootRequest(AsyncWebServerRequest *request);
-        void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
-        void notifyClients(String message); 
-        void onEvent(AsyncWebSocket *server,
-                     AsyncWebSocketClient *client,
-                     AwsEventType type,
-                     void *arg,
-                     uint8_t *data,
-                     size_t len);
-        void onRootRequest(AsyncWebServerRequest *request);
-        AsyncWebServer *_server = NULL;
-        AsyncWebSocket *_ws = NULL;
-};
+// WiFi network credentials
+const char* WIFI_SSID = "TALKTALK42E7AF";
+const char* WIFI_PASSWORD = ;
+
+void serverInitWiFi();
+
+void serverInitWebServer();
+
+void serverInitWebSocket();
+
+void serverOnRootRequest(AsyncWebServerRequest *request);
+
+void serverNotifyWebsocketClients(String message);
+
+void serverWebsocketOnEvent(AsyncWebSocket *server,
+                            AsyncWebSocketClient *client,
+                            AwsEventType type,
+                            void *arg,
+                            uint8_t *data,
+                            size_t len);
+
+void serverHandleWebSocketMessage(void *arg, uint8_t *data, size_t len);
 
 #endif /* SERVER_H_ */
